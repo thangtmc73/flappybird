9672f4b0bbedb23fa14bde28fb79f93850af3248
thangtmc73 <thangtmc73@gmail.com>
2021-01-02 16:08:09 +0700

    feat(Resources): read image and store image data to sprites dictionary

--- main.go
+++ main.go
@@ -6,10 +6,12 @@ import (
 	"github.com/hajimehoshi/ebiten/v2"
 	"github.com/thangtmc73/flappybird/config"
 	"github.com/thangtmc73/flappybird/game"
+	"github.com/thangtmc73/flappybird/resources"
 )
 
 // NewGame init new game
 func NewGame() *game.Game {
+	resources.Init()
 	g := &game.Game{}
 	g.Init()
 	return g
---
+++ resources/resources.go
@@ -0,0 +1,41 @@
+package resources
+
+import (
+	"image/png"
+	"log"
+	"os"
+
+	"github.com/hajimehoshi/ebiten/v2"
+)
+
+var (
+	// mapSpriteByKey contains data loaded from image
+	mapSpriteByKey map[string]*ebiten.Image
+)
+
+func loadImageToSprite(path string, key string) {
+	f, err := os.Open(path)
+	if err != nil {
+		log.Fatal(err)
+	}
+	img, err := png.Decode(f)
+	if err != nil {
+		log.Fatal(err)
+	}
+	mapSpriteByKey[key] = ebiten.NewImageFromImage(img)
+}
+
+// Init to initialize resources
+func Init() {
+	mapSpriteByKey = make(map[string]*ebiten.Image)
+	loadImageToSprite("resources/bird.png", "bird")
+}
+
+// GetSpriteByKey allow getting resource with key
+func GetSpriteByKey(key string) *ebiten.Image {
+	sprite, ok := mapSpriteByKey[key]
+	if ok {
+		return sprite
+	}
+	return nil
+}
