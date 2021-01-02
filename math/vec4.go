14c90a802846e06ac120aaf171a4cdc5adf2a515
thangtmc73 <thangtmc73@gmail.com>
2021-01-02 16:31:23 +0700

    chore(code): remove unused code

--- math/vec4.go
+++ math/vec4.go
@@ -1,7 +1,5 @@
 package math
 
-import "fmt"
-
 type Vec4 struct {
 	X float64
 	Y float64
@@ -15,6 +13,5 @@ func (vec *Vec4) Transform(mat Mat4) *Vec4 {
 	result.Y = (vec.X * mat.C12) + (vec.Y * mat.C22) + (vec.Z * mat.C32) + (vec.W * mat.C42)
 	result.Z = (vec.X * mat.C13) + (vec.Y * mat.C23) + (vec.Z * mat.C33) + (vec.W * mat.C43)
 	result.W = (vec.X * mat.C14) + (vec.Y * mat.C24) + (vec.Z * mat.C34) + (vec.W * mat.C44)
-	fmt.Println(result.X, result.Y, result.Z, mat.C22)
 	return result
 }
