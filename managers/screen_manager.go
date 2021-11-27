fe63d740a5713e7ad68b76d83d4ba2b16643cc0f
thangtm2 <thangtm2@vng.com.vn>
2021-11-27 16:15:23 +0700

    feat: add screen manager

---
+++ managers/screen_manager.go
@@ -0,0 +1,49 @@
+package managers
+
+import (
+	"github.com/thangtmc73/flappybird/objects"
+)
+
+type ScreenManager struct {
+	Stack []*objects.ScreenHandler
+}
+
+func NewScreenManager() *ScreenManager {
+	manager := &ScreenManager{}
+	return manager
+}
+
+func (s *ScreenManager) Back() (*objects.ScreenHandler, int) {
+	stackLen := len(s.Stack)
+	if stackLen == 0 {
+		return nil, -1
+	}
+	index := stackLen - 1
+	return s.Stack[index], index
+}
+
+func (s *ScreenManager) IsEmpty() bool {
+	return len(s.Stack) == 0
+}
+
+func (s *ScreenManager) AddScreen(screen *objects.ScreenHandler) {
+	s.Stack = append(s.Stack, screen)
+	newScreen, _ := s.Back()
+	if newScreen == nil {
+		return
+	}
+	if !(*newScreen).Initialized() {
+		(*newScreen).Init()
+	}
+}
+
+func (s *ScreenManager) RemoveScreen() bool {
+	if s.IsEmpty() {
+		return false
+	}
+
+	currentScreen, index := s.Back()
+	(*currentScreen).Destroy()
+	s.Stack = s.Stack[:index]
+	return true
+}
